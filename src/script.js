fc59a461e9de3f24788cd6444bbce9b8c11350c1
Antônio Rafael <antonio.rafael0415@gmail.com>
2025-11-12 20:19:02 -0300

    :tada: Initial Commit

---
+++ src/script.js
@@ -0,0 +1,18 @@
+// Criar a função de toggle entre modo claro e modo escuro 
+function toggleMode() {
+    const html = document.documentElement
+    const img = document.querySelector('#profile img')
+
+    html.classList.toggle('light') // Alterna entre claro e escuro
+
+    // Condicional para alterar a imagem de perfil
+    if (html.classList.contains('light')) {
+
+        img.setAttribute('src', '../assets/avatar-light.png')
+    } else {
+
+        img.setAttribute('src', '../assets/avatar.png')
+
+    }
+}
+
